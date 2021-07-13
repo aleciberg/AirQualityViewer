959a6dec106f1d920979ef8502f2205719092268
Alec Iberg <ibergalec@gmail.com>
2021-07-13 16:53:24 -0700

    added degree symbol

--- views/searchResults.js
+++ views/searchResults.js
@@ -4,8 +4,13 @@ module.exports = (weatherData) => {
   return layout(
     `
             <div>
-                <h2>Search results for ${weatherData.city}, ${weatherData.state}</h2>
+                <h2>Search results for ${weatherData.city}, ${
+      weatherData.state
+    }</h2>
                 <h2>Air Quality: ${weatherData.current.pollution.aqius}</h2>
+                <h2>Temperature: ${
+                  (weatherData.current.weather.tp * 9) / 5 + 32
+                } &#8457;</h2>
             </div>
         `
   );
